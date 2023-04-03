e3694b9894c3cc31c2742b350298aaa91559d6a3
M. Iqbal Effendi <iqbaleff214@gmail.com>
2023-04-03 21:25:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simolen
+
+## Tentang Simolen
+
+Simolen adalah ....
+
+## Cara Instal
+
+- Buka CLI, posisikan pada direktori yang diinginkan
+- _Clone_ repo ini
+- Jalankan perintah berikut untuk menjalankan _web server_ milik Laravel
+    ```
+    php artisan serve
+    ```
+  
+## _Credit_
+Proyek ini dikembangkan menggunakan Laravel dan _template_ [Dashtreme](https://themewagon.com/themes/free-bootstrap-4-html5-admin-dashboard-template-dashtreme/)
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Simolen</title>
+    <!-- loader-->
+    <link href="/assets/css/pace.min.css" rel="stylesheet"/>
+    <script src="/assets/js/pace.min.js"></script>
+    <!--favicon-->
+    <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
+    <!-- Vector CSS -->
+    <link href="/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+    <!-- simplebar CSS-->
+    <link href="/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
+    <!-- Bootstrap core CSS-->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <!-- animate CSS-->
+    <link href="/assets/css/animate.css" rel="stylesheet" type="text/css"/>
+    <!-- Icons CSS-->
+    <link href="/assets/css/icons.css" rel="stylesheet" type="text/css"/>
+    <!-- Sidebar CSS-->
+    <link href="/assets/css/sidebar-menu.css" rel="stylesheet"/>
+    <!-- Custom Style-->
+    <link href="/assets/css/app-style.css" rel="stylesheet"/>
+
+</head>
+
+<body class="bg-theme bg-theme4">
+
+<!-- Start wrapper-->
+<div id="wrapper">
+
+    <!--Start sidebar-wrapper-->
+    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true" class="ml-2" style="margin-top: 40px">
+        <div class="avatar mx-auto text-center mt-5">
+            <img class="align-self-start mx-auto" style="border-radius: 50%" src="https://via.placeholder.com/110x110"
+                 alt="user avatar">
+        </div>
+        <div class="text-center my-4">
+            <p>Panitia Pelaksana</p>
+            <h5>John Doe</h5>
+        </div>
+        <hr>
+        <ul class="sidebar-menu do-nicescrol p-3">
+            <li class="sidebar-header">MENU UTAMA</li>
+            <li><a href="#"><i class="zmdi zmdi-view-dashboard"></i> <span>Dashboard</span></a></li>
+            <li><a href="#"><i class="zmdi zmdi-invert-colors"></i> <span>Manajemen Montir</span></a></li>
+            <li><a href="#"><i class="zmdi zmdi-card"></i> <span>Penjualan</span></a></li>
+            <li><a href="#"><i class="zmdi zmdi-archive"></i> <span>Riwayat</span></a></li>
+            <li><a href="#"><i class="zmdi zmdi-account-box-phone"></i> <span>Profil Bengkel</span></a></li>
+
+
+            <li class="sidebar-header">LAINNYA</li>
+            <li><a href="#"><i class="zmdi zmdi-run"></i> <span>Log Out</span></a></li>
+        </ul>
+    </div>
+    <!--End sidebar-wrapper-->
+
+    <div class="clearfix"></div>
+
+    <div class="content-wrapper" style="padding-top: 52px">
+        <div class="container-fluid">
+            <!--Start Dashboard Content-->
+            <h3 class="pb-3">DASHBOARD</h3>
+
+            <div class="card mt-3">
+                <div class="card-content">
+                    <div class="row row-group m-0">
+                        <div class="col-12 col-lg-6 col-xl-3 border-light">
+                            <div class="card-body">
+                                <h5 class="text-white mb-0">
+                                    729 <span class="float-right"><i class="fa fa-shopping-cart"></i></span>
+                                </h5>
+                                <div class="progress my-3" style="height:3px;">
+                                    <div class="progress-bar" style="width:55%"></div>
+                                </div>
+                                <p class="mb-0 text-white small-font">
+                                    Total Pemesanan <span class="float-right">+4.2% <i class="zmdi zmdi-long-arrow-up"></i></span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 col-xl-3 border-light">
+                            <div class="card-body">
+                                <h5 class="text-white mb-0">
+                                    Rp320.000 <span class="float-right"><i class="fa fa-usd"></i></span>
+                                </h5>
+                                <div class="progress my-3" style="height:3px;">
+                                    <div class="progress-bar" style="width:55%"></div>
+                                </div>
+                                <p class="mb-0 text-white small-font">
+                                    Total Penjualan <span class="float-right">+1.2% <i class="zmdi zmdi-long-arrow-up"></i></span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 col-xl-3 border-light">
+                            <div class="card-body">
+                                <h5 class="text-white mb-0">
+                                    5/7 <span class="float-right"><i class="fa fa-users"></i></span>
+                                </h5>
+                                <div class="progress my-3" style="height:3px;">
+                                    <div class="progress-bar" style="width:55%"></div>
+                                </div>
+                                <p class="mb-0 text-white small-font">
+                                    Montir Tersedia
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 col-xl-3 border-light">
+                            <div class="card-body">
+                                <h5 class="text-white mb-0">
+                                    4.9/5.0 <span class="float-right"><i class="fa fa-star"></i></span>
+                                </h5>
+                                <div class="progress my-3" style="height:3px;">
+                                    <div class="progress-bar" style="width:55%"></div>
+                                </div>
+                                <p class="mb-0 text-white small-font">
+                                    Rating Consumer <span class="float-right">+5.2% <i class="zmdi zmdi-long-arrow-up"></i></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-lg-8 col-xl-8">
+                    <div class="card">
+                        <div class="card-header">Laporan Statistik
+                            <div class="card-action">
+                                <div class="dropdown">
+                                    <a href="javascript:void();" class="dropdown-toggle dropdown-toggle-nocaret"
+                                       data-toggle="dropdown">
+                                        <i class="icon-options"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a class="dropdown-item" href="javascript:void();">Action</a>
+                                        <a class="dropdown-item" href="javascript:void();">Another action</a>
+                                        <a class="dropdown-item" href="javascript:void();">Something else here</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="javascript:void();">Separated link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <i class="fa fa-circle mr-2 text-white"></i>Pemesanan
+                                </li>
+                                <li class="list-inline-item">
+                                    <i class="fa fa-circle mr-2 text-light"></i>Penjualan
+                                </li>
+                            </ul>
+                            <div class="chart-container-1">
+                                <canvas id="chart1"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-4 col-xl-4">
+                    <div class="card">
+                        <div class="card-header">Penjualan Minggu Ini
+                            <div class="card-action">
+                                <div class="dropdown">
+                                    <a href="javascript:void();" class="dropdown-toggle dropdown-toggle-nocaret"
+                                       data-toggle="dropdown">
+                                        <i class="icon-options"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a class="dropdown-item" href="javascript:void();">Action</a>
+                                        <a class="dropdown-item" href="javascript:void();">Another action</a>
+                                        <a class="dropdown-item" href="javascript:void();">Something else here</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="javascript:void();">Separated link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="chart-container-2">
+                                <canvas id="chart2"></canvas>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table align-items-center">
+                                <tbody>
+                                <tr>
+                                    <td><i class="fa fa-circle text-white mr-2"></i> Yamalube</td>
+                                    <td>Rp520.000</td>
+                                    <td>+55%</td>
+                                </tr>
+                                <tr>
+                                    <td><i class="fa fa-circle text-light-1 mr-2"></i>Castrol</td>
+                                    <td>Rp430.500</td>
+                                    <td>+25%</td>
+                                </tr>
+                                <tr>
+                                    <td><i class="fa fa-circle text-light-2 mr-2"></i>FDR</td>
+                                    <td>Rp420.000</td>
+                                    <td>-10%</td>
+                                </tr>
+                                <tr>
+                                    <td><i class="fa fa-circle text-light-3 mr-2"></i>Swallow Tube</td>
+                                    <td>Rp395.000</td>
+                                    <td>-25%</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div><!--End Row-->
+
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div class="card">
+                        <div class="card-header">Pemesanan Terakhir
+                            <div class="card-action">
+                                <div class="dropdown">
+                                    <a href="javascript:void();" class="dropdown-toggle dropdown-toggle-nocaret"
+                                       data-toggle="dropdown">
+                                        <i class="icon-options"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a class="dropdown-item" href="javascript:void();">Action</a>
+                                        <a class="dropdown-item" href="javascript:void();">Another action</a>
+                                        <a class="dropdown-item" href="javascript:void();">Something else here</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="javascript:void();">Separated link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table align-items-center table-flush table-borderless">
+                                <thead>
+                                <tr>
+                                    <th>Montir</th>
+                                    <th>Status</th>
+                                    <th>Waktu Pesan</th>
+                                    <th>Lokasi</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        <img src="https://via.placeholder.com/110x110" class="product-img mr-4" alt="product img">Vladimir Putin
+                                    </td>
+                                    <td>
+                                        <i class="fa fa-circle mr-2 text-danger"></i> Proses
+                                    </td>
+                                    <td>03/04/2023 23:00:00</td>
+                                    <td>
+                                        Jalan Pegangsaan Timur
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="https://via.placeholder.com/110x110" class="product-img mr-4" alt="product img">Kim Jong-Un
+                                    </td>
+                                    <td>
+                                        <i class="fa fa-circle mr-2 text-success"></i> Selesai
+                                    </td>
+                                    <td>01/04/2023 13:00:00</td>
+                                    <td>
+                                        Jalan Rengasdengklok
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img src="https://via.placeholder.com/110x110" class="product-img mr-4" alt="product img">Naruhito
+                                    </td>
+                                    <td>
+                                        <i class="fa fa-circle mr-2 text-success"></i> Selesai
+                                    </td>
+                                    <td>29/03/2023 09:30:00</td>
+                                    <td>
+                                        Jalan Sesama
+                                    </td>
+                                </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div><!--End Row-->
+
+            <!--End Dashboard Content-->
+
+            <!--start overlay-->
+            <div class="overlay toggle-menu"></div>
+            <!--end overlay-->
+
+        </div>
+        <!-- End container-fluid-->
+
+    </div><!--End content-wrapper-->
+    <!--Start Back To Top Button-->
+    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+    <!--End Back To Top Button-->
+
+    <!--Start footer-->
+    <footer class="footer">
+        <div class="container">
+            <div class="text-center">
+                Copyright © 2023 Simelon
+            </div>
+        </div>
+    </footer>
+    <!--End footer-->
+
+</div><!--End wrapper-->
+
+<!-- Bootstrap core JavaScript-->
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/popper.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+
+<!-- simplebar js -->
+<script src="/assets/plugins/simplebar/js/simplebar.js"></script>
+<!-- sidebar-menu js -->
+<script src="/assets/js/sidebar-menu.js"></script>
+<!-- loader scripts -->
+<script src="/assets/js/jquery.loading-indicator.js"></script>
+<!-- Custom scripts -->
+<script src="/assets/js/app-script.js"></script>
+<!-- Chart js -->
+
+<script src="/assets/plugins/Chart.js/Chart.min.js"></script>
+
+<!-- Index js -->
+<script src="/assets/js/index.js"></script>
+
+
+</body>
+</html>
